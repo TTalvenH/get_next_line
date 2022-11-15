40a59c3e4db6cb8240e3365d703b209b2f1c93ee
Tuomo <tuomo.talvenheimo@outlook.com>
2022-11-15 23:15:26 +0200

    not working, have to free stash

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,87 @@
 #include "get_next_line.h"
 
+
+//get_next_line
+//read_line
+//copy
+//vec_init_or_grow -> ft_bzero
+//ft_strlcat
+
+int		vec_init_or_grow(t_vec *stash, size_t new_size)
+{
+	if (stash->used == 0)
+	{
+		stash->str = malloc(sizeof(char) * (new_size + 1) ); //need + 1?
+		if (!stash->str)
+			return (-1);
+		stash->str[new_size] = '\0';
+		ft_bzero(stash->str, new_size);
+		return (0);
+	}
+	else   // maybe only need this ?
+	{
+		char *tmp;
+		tmp = malloc (sizeof(char) * new_size + 1); // need + 1?
+		if (!tmp)
+			return (-1);
+		tmp[new_size] = '\0';
+		strncpy(tmp, stash->str, stash->used);
+		free(stash->str);
+		stash->str = tmp;
+	}
+	return (0);
+}
+
+char	*copy(char *buff, int read_rtn)
+{
+	static t_vec stash =(t_vec){NULL, 0, 0};
+	char	*rtn;
+
+	rtn = NULL;
+	if (vec_init_or_grow(&stash, stash.used + read_rtn) < 0)
+		return (NULL);
+	stash.used = stash.used + read_rtn;
+	ft_strlcat(stash.str, buff, stash.used + 1);
+	while (stash.index < stash.used)
+	{
+		if (stash.str[stash.index] == '\n')
+			return (ft_strndup(stash.str, stash.index)); //if return, then its the newline return string.
+		stash.index++;
+	}
+	return (rtn);
+}
+
+char	*read_line(int fd)
+{
+	char	*buff;
+	int		read_rtn;
+
+	buff = malloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	buff[BUFF_SIZE] = '\0';
+	read_rtn = read(fd, buff, BUFF_SIZE);
+	return (copy(buff, read_rtn));
+}
+
+
 char *get_next_line(int fd)
 {
-	void	*buff;
-	
+
+	if(fd < 0)
+		return (NULL);
+	return (read_line(fd));
 }
+
+
 
 int main()
 {
-	int fd
-	open("text.txt", O_RDONLY);
-	for (int i = 0, i < 4, i++)
+	int fd;
+	fd = open("text.txt", O_RDONLY);
+	for (int i = 0; i < 3; i++)
 	{
 		printf("%s", get_next_line(fd));
 	}
-	return (0)
+		return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,25 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
+ 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h> //*** POISTA **********************
 #include <fcntl.h> //*** POISTA *********************
+#include <string.h>//*** POISTA *********************
+typedef struct s_vec
+{
+	char	*str;
+	size_t	used;
+	size_t	index;
+}t_vec;
 
 char *get_next_line(int fd);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strndup(const char *s1, size_t n);
+
 
 #endif
