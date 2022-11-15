2ba2027feefbb95779cda071b05466b2e36f5e59
Tuomo <Tuomo.talvenheimo@outlook.com>
2022-11-15 18:29:58 +0200

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,18 @@
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	void	*buff;
+	
+}
+
+int main()
+{
+	int fd
+	open("text.txt", O_RDONLY);
+	for (int i = 0, i < 4, i++)
+	{
+		printf("%s", get_next_line(fd));
+	}
+	return (0)
+}
