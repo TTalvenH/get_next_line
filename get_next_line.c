0b6c57444bfe6c399b2c852b0162adeeb8e61b5d
Tuomo Talvenheimo <ttalvenh@c1r4p3.hive.fi>
2022-11-18 16:57:23 +0200

    fixing..

--- get_next_line.c
+++ get_next_line.c
@@ -26,79 +26,84 @@ int		vec_init_or_grow(t_vec *stash, size_t new_size)
 }
 
 
-int		stash_cut(t_vec *stash)
+char	*stash_cut(t_vec *stash)
 {
 	char *tmp;
+	char *rtn;
+
+	rtn = ft_strndup(stash->str, stash->index + 1);
 	tmp = malloc (sizeof(char) * (stash->used - stash->index));
 	if (!tmp)
-		return (-1);
-	tmp[stash->used - stash->index] = '\0';
+		return (NULL);
+	tmp[stash->used - (stash->index + 1)] = '\0';
 	ft_strlcpy(tmp, &stash->str[stash->index + 1], stash->used - stash->index + 1);
 	stash->used = stash->used - (stash->index + 1);
 	stash->index = 0;
-	free (stash->str);
+	//free (stash->str);
 	stash->str = tmp;
-
-	return (0);
+	return (rtn);
 }
+
 char	*copy(char *buff, int read_rtn)
 {
 	static t_vec stash =(t_vec){NULL, 0, 0};
 
-	if (stash.used > 0 && stash.used != (stash.index + 1) )
-	{
-		if (stash_cut(&stash) < 0)
-			return (NULL);
-	}
+	//if (stash.used > 0 && stash.used != (stash.index + 1) )
+	//{
+	//	if (stash_cut(&stash) < 0)
+	//		return (NULL);
+	//}
 	if (read_rtn)
 	{
 		if (vec_init_or_grow(&stash, stash.used + read_rtn) < 0)
 			return (NULL);
 		stash.used = stash.used + read_rtn;
 		ft_strlcat(stash.str, buff, stash.used + 1);
+		//free (buff);
 	}
-	while (stash.index < stash.used)
+	if (stash.index + 1 == stash.used && !read_rtn)
+		return (NULL);
+	while ((stash.index) != stash.used)
 	{
-		if (stash.str[stash.index] == '\n'|| (stash.index + 1) == stash.used)
-			return (ft_strndup(stash.str, stash.index + 1)); //if return, then its the newline return string.
+		if (stash.str[stash.index] == '\n' || (stash.index + 1 == stash.used && !read_rtn))
+			return (stash_cut(&stash)); //if return, then its the newline return string.
 		stash.index++;
 	}
 	return (NULL);
-
 }
 
-char	*read_line(int fd)
+char *get_next_line(int fd)
 {
 	char	*buff;
+	char	*rtn;
 	int		read_rtn;
 
+	read_rtn = 1;
+	rtn = NULL;
 	buff = malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (!buff)
 		return (NULL);
 	buff[BUFF_SIZE] = '\0';
-	read_rtn = read(fd, buff, BUFF_SIZE);
-	return (copy(buff, read_rtn));
-}
-
-
-char *get_next_line(int fd)
-{
-
-	if(fd < 0)
-		return (NULL);
-	return (read_line(fd));
-}
-
-
+	while (!rtn && read_rtn)
+	{
+		read_rtn = read(fd, buff, BUFF_SIZE);
+		rtn = copy(buff, read_rtn);
+	}
+	return (rtn);
+}	
 
 int main()
 {
 	int fd;
-	
+	char	*str;
 	fd = open("text.txt", O_RDONLY);
 		for (int i = 0; i < 7; i++)
 	{
-		printf("%s", get_next_line(fd));
+		str = get_next_line(fd);
+		printf("%s", str);
+		//free (str);
 	}
-		return (0);
+	system("export MallocStackLogging=1");
+    system("leaks get_next_line");
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 16
+# define BUFF_SIZE 2
  
 #include <stdlib.h>
 #include <unistd.h>
