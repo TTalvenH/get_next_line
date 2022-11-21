4e6a16cd5161154501834e6223298e930af5cf71
Tuomo <Tuomo.talvenheimo@outlook.com>
2022-11-21 20:27:43 +0200

    1st working build, improvements needed

--- get_next_line.c
+++ get_next_line.c
@@ -4,17 +4,17 @@ int		vec_init_or_grow(t_vec *stash, size_t new_size)
 {
 	if (stash->used == 0)
 	{
-		stash->str = malloc(sizeof(char) * (new_size + 1) ); //need + 1?
+		free(stash->str);
+		stash->str = malloc(sizeof(char) * (new_size + 1) );
 		if (!stash->str)
 			return (-1);
 		stash->str[new_size] = '\0';
-		ft_bzero(stash->str, new_size);
-		return (0);
+			return (0);
 	}
-	else   // maybe only need this ?
+	else
 	{
 		char *tmp;
-		tmp = malloc (sizeof(char) * new_size + 1); // need + 1?
+		tmp = malloc (sizeof(char) * new_size + 1);
 		if (!tmp)
 			return (-1);
 		tmp[new_size] = '\0';
@@ -37,9 +37,9 @@ char	*stash_cut(t_vec *stash)
 		return (NULL);
 	tmp[stash->used - (stash->index + 1)] = '\0';
 	ft_strlcpy(tmp, &stash->str[stash->index + 1], stash->used - stash->index + 1);
+	free(stash->str);
 	stash->used = stash->used - (stash->index + 1);
 	stash->index = 0;
-	//free (stash->str);
 	stash->str = tmp;
 	return (rtn);
 }
@@ -48,26 +48,23 @@ char	*copy(char *buff, int read_rtn)
 {
 	static t_vec stash =(t_vec){NULL, 0, 0};
 
-	//if (stash.used > 0 && stash.used != (stash.index + 1) )
-	//{
-	//	if (stash_cut(&stash) < 0)
-	//		return (NULL);
-	//}
 	if (read_rtn)
 	{
 		if (vec_init_or_grow(&stash, stash.used + read_rtn) < 0)
 			return (NULL);
 		stash.used = stash.used + read_rtn;
-		ft_strlcat(stash.str, buff, stash.used + 1);
-		//free (buff);
+		ft_strlcat(stash.str, buff, stash.used + 1, stash.used - read_rtn);
 	}
-	if (stash.index + 1 == stash.used && !read_rtn)
-		return (NULL);
-	while ((stash.index) != stash.used)
+	while (stash.index < stash.used)
 	{
 		if (stash.str[stash.index] == '\n' || (stash.index + 1 == stash.used && !read_rtn))
-			return (stash_cut(&stash)); //if return, then its the newline return string.
+			return (stash_cut(&stash)); 
 		stash.index++;
+	}
+	if(!read_rtn)
+	{
+		free (stash.str);
+		stash.index = 0;
 	}
 	return (NULL);
 }
@@ -89,21 +86,24 @@ char *get_next_line(int fd)
 		read_rtn = read(fd, buff, BUFF_SIZE);
 		rtn = copy(buff, read_rtn);
 	}
+	free(buff);
 	return (rtn);
 }	
 
 int main()
 {
-	int fd;
-	char	*str;
+	int fd = 0;
+	char	*str = NULL;
+	int	i = 1;
 	fd = open("text.txt", O_RDONLY);
-		for (int i = 0; i < 7; i++)
+	while(str || i--)
 	{
 		str = get_next_line(fd);
-		printf("%s", str);
-		//free (str);
+		if (str)
+			printf("%s", str);
+		free(str);
 	}
-	system("export MallocStackLogging=1");
-    system("leaks get_next_line");
+	//system("export MallocStackLogging=1");
+    //system("leaks get_next_line");
 	return (0);
 }
