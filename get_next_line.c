a0d99ef190b123ba999672bc96b8ee486d1482ff
Tuomo Talvenheimo <ttalvenh@c1r4p3.hive.fi>
2022-11-24 13:37:41 +0200

    working build, norminette OK

--- get_next_line.c
+++ get_next_line.c
@@ -1,118 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttalvenh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 13:07:14 by ttalvenh          #+#    #+#             */
+/*   Updated: 2022/11/24 13:28:47 by ttalvenh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int		vec_init_or_grow(t_vec *stash, size_t new_size)
+int	vec_init_or_grow(t_vec *vec, size_t new_size)
 {
-	if (stash->used == 0)
+	char	*tmp;
+
+	if (vec->u == 0)
 	{
-		free(stash->str);
-		stash->str = malloc(sizeof(char) * (new_size + 1) );
-		if (!stash->str)
+		if (vec->str)
+			free(vec->str);
+		vec->str = malloc(sizeof(char) * (new_size + 1));
+		if (!vec->str)
 			return (-1);
-		stash->str[new_size] = '\0';
-			return (0);
+		vec->str[new_size] = '\0';
+		return (0);
 	}
 	else
 	{
-		char *tmp;
 		tmp = malloc (sizeof(char) * new_size + 1);
 		if (!tmp)
 			return (-1);
 		tmp[new_size] = '\0';
-		ft_strlcpy(tmp, stash->str, stash->used + 1);
-		free(stash->str);
-		stash->str = tmp;
+		ft_strlcpy(tmp, vec->str, vec->u + 1);
+		free(vec->str);
+		vec->str = NULL;
+		vec->str = tmp;
 	}
 	return (0);
 }
 
-
-char	*stash_cut(t_vec *stash)
+char	*vec_free(t_vec *vec)
 {
-	char *tmp;
-	char *rtn;
-
-	rtn = ft_strndup(stash->str, stash->index + 1);
-	tmp = malloc (sizeof(char) * (stash->used - stash->index));
-	if (!tmp)
-		return (NULL);
-	tmp[stash->used - (stash->index + 1)] = '\0';
-	ft_strlcpy(tmp, &stash->str[stash->index + 1], stash->used - stash->index + 1);
-	free(stash->str);
-	stash->used = stash->used - (stash->index + 1);
-	stash->index = 0;
-	stash->str = tmp;
-	return (rtn);
-}
-
-char	*copy(char *buff, int read_rtn)
-{
-	static t_vec stash =(t_vec){NULL, 0, 0};
 	char	*rtn;
 
+	rtn = ft_strndup(vec->str, vec->i);
+	free (vec->str);
+	vec->str = NULL;
+	vec->u = 0;
+	return (rtn);
+}
+
+char	*vec_cut(t_vec *vec)
+{
+	char	*tmp;
+	char	*rtn;
+
+	rtn = ft_strndup(vec->str, vec->i + 1);
+	tmp = malloc (sizeof(char) * (vec->u - vec->i));
+	if (!tmp)
+		return (NULL);
+	tmp[vec->u - (vec->i + 1)] = '\0';
+	ft_strlcpy(tmp, &vec->str[vec->i + 1], vec->u - vec->i + 1);
+	free(vec->str);
+	vec->str = NULL;
+	vec->u = vec->u - (vec->i + 1);
+	vec->i = 0;
+	vec->str = tmp;
+	return (rtn);
+}
+
+char	*copy(char *buff, int r)
+{
+	static t_vec	vec = {NULL, 0, 0};
+	char			*rtn;
+
 	rtn = NULL;
-	if (read_rtn)
+	if (r)
 	{
-		if (vec_init_or_grow(&stash, stash.used + read_rtn) < 0)
+		if (vec_init_or_grow(&vec, vec.u + r) < 0)
 			return (NULL);
-		stash.used = stash.used + read_rtn;
-		ft_strlcat(stash.str, buff, stash.used + 1, stash.used - read_rtn);
+		vec.u = vec.u + r;
+		ft_strlcat(vec.str, buff, vec.u + 1, vec.u - r);
 	}
-	while (stash.index < stash.used )
+	while (vec.i < vec.u)
 	{
-		if (stash.str[stash.index] == '\n' || (stash.index + 1 == stash.used && read_rtn < BUFF_SIZE))
-			return (stash_cut(&stash)); 
-		stash.index++;
+		if (vec.str[vec.i] == '\n' || (vec.i + 1 == vec.u && r < BUFF_SIZE))
+			return (vec_cut(&vec));
+		vec.i++;
 	}
-	if(!read_rtn)
+	if (!r)
 	{
-		if (stash.str)
-			rtn = stash.str;
-		free (stash.str);
-		stash.index = 0;
+		if (vec.str)
+			rtn = vec_free(&vec);
+		vec.i = 0;
 	}
 	return (rtn);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	char	*buff;
 	char	*rtn;
-	int		read_rtn;
+	int		r;
 
-	read_rtn = 1;
+	r = 1;
 	rtn = NULL;
 	buff = malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (!buff)
 		return (NULL);
 	buff[BUFF_SIZE] = '\0';
-	while (!rtn && read_rtn)
+	while (!rtn && r)
 	{
-		read_rtn = read(fd, buff, BUFF_SIZE);
-		if (read_rtn < 0)
+		r = read(fd, buff, BUFF_SIZE);
+		if (r < 0)
 		{
 			free (buff);
+			buff = NULL;
 			return (NULL);
 		}
-		rtn = copy(buff, read_rtn);
+		rtn = copy(buff, r);
 	}
 	free(buff);
 	return (rtn);
-}	
-
-int main()
-{
-	int fd = 0;
-	char	*str = NULL;
-	int	i = 15;
-	fd = open("42_no_nl", O_RDONLY);
-	while(str || i--)
-	{
-		str = get_next_line(fd);
-		if (str)
-			printf("%s", str);
-		free(str);
-	}
-	// system("export MallocStackLogging=1");
-    // system("leaks get_next_line");
-	return (0);
 }
