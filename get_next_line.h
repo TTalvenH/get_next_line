3fcb1b71ac1c8b98e6895cc9043fb0102fce0014
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2022-11-24 13:37:41 +0200

    working build, norminette OK

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttalvenh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 13:07:26 by ttalvenh          #+#    #+#             */
+/*   Updated: 2022/11/24 13:29:33 by ttalvenh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 42
- 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h> //*** POISTA **********************
-#include <fcntl.h> //*** POISTA *********************
+
+# include <stdlib.h>
+# include <unistd.h>
+
 typedef struct s_vec
 {
 	char	*str;
-	size_t	used;
-	size_t	index;
+	size_t	u;
+	size_t	i;
 }t_vec;
 
-char 	*get_next_line(int fd);
+char	*get_next_line(int fd);
 void	ft_strlcat(char *dst, const char *src, size_t dstsize, size_t dst_len);
 void	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strndup(const char *s1, size_t n);
-
-
 #endif
